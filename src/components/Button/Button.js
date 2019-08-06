5e4b6d4b1fa3a0d3ff9b60e8715cecee80d8f544
Sylwia Fait <sylwia.fait@gmail.com>
2019-08-06 11:46:36 +0200

    Add styled-components

---
+++ src/components/Button/Button.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    background: blue;
+`;
+
+export default Button;
---
+++ src/views/Root/Root.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../../components/Button/Button';
+
+const Root = () => (
+  <Button>Hello</Button>
+);
+
+export default Root;
