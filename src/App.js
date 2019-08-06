7122dbc4032829b0b60f488c8dca45ab0d693c28
Sylwia Fait <sylwia.fait@gmail.com>
2019-08-06 10:50:28 +0200

    New project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import './normalize.css';
-import './index.css';
-import Header from './components/Header/Header';
+import logo from './logo.svg';
+import './App.css';
 
 const App = () => (
-    <>
-        <header>
-            <Header />
-        </header>
-    </>
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
 );
 
 export default App;
